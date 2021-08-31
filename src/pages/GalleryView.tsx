12e3f06c413e7de7b7d989b7c4ade48a2364e37f
SU06-LT27B <11609005+SU06-LT27B@users.noreply.github.com>
2021-08-31 23:51:12 +0900

    크롤링 코드 추가

--- src/pages/GalleryView.tsx
+++ src/pages/GalleryView.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const GalleryView = () => {
+    const galleryId = "ani1_new1";
+    const page = 1;
+    const url = `https://gall.dcinside.com/board/lists/?id=${galleryId}&page=${page}`;
+
+    const { data, error, mutate } = useSWR(url, fetcher);
+
+    console.log(data);
+
     return (
         <div>
             <span>test</span>
